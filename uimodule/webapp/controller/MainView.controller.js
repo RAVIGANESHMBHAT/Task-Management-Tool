8f590b129b4089d0ecd282b12a777840685532d7
I524764 <raviganesh.maniyoor@sap.com>
2022-03-19 13:01:30 +0530

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/ravi/dissertation/TaskManagementTool/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.ravi.dissertation.TaskManagementTool.controller.MainView", {});
+});
