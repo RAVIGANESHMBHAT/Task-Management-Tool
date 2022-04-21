515b17495066d96d35a0915e86171bf40c3b5e5d
Raviganesh M <raviganeshmbhat999@gmail.com>
2022-04-21 16:18:08 +0530

    Added Flexible column layout

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,28 +1,55 @@
 sap.ui.define(
-    ["sap/ui/core/UIComponent", "sap/ui/Device", "com/ravi/dissertation/TaskManagementTool/model/models"],
-    function (UIComponent, Device, models) {
-        "use strict";
+  [
+    "sap/base/util/UriParameters",
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/f/library",
+    "sap/f/FlexibleColumnLayoutSemanticHelper",
+    "sap/ui/Device",
+    "com/ravi/dissertation/TaskManagementTool/model/models",
+  ],
+  function (UriParameters, UIComponent, JSONModel, library, FlexibleColumnLayoutSemanticHelper,  Device, models) {
+    "use strict";
 
-        return UIComponent.extend("com.ravi.dissertation.TaskManagementTool.Component", {
-            metadata: {
-                manifest: "json"
-            },
+    const LayoutType = library.LayoutType;
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+    return UIComponent.extend("com.ravi.dissertation.TaskManagementTool.Component", {
+      metadata: {
+        manifest: "json",
+        interfaces: ["sap.ui.core.IAsyncContentCreation"]
+      },
 
-                // enable routing
-                this.getRouter().initialize();
+      /**
+       * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+       * @public
+       * @override
+       */
+      init: function () {
+        // call the base component's init function
+        UIComponent.prototype.init.apply(this, arguments);
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
+        // enable routing
+        this.getRouter().initialize();
+
+        const oModel = new JSONModel();
+        this.setModel(oModel);
+
+        // set the device model
+        this.setModel(models.createDeviceModel(), "device");
+      },
+
+      getHelper: function () {
+        var oFCL = this.getRootControl().byId("fcl"),
+          oParams = UriParameters.fromQuery(location.search),
+          oSettings = {
+            defaultTwoColumnLayoutType: LayoutType.TwoColumnsMidExpanded,
+            mode: oParams.get("mode"),
+            initialColumnsCount: oParams.get("initial"),
+            maxColumnsCount: oParams.get("max")
+          };
+
+        return FlexibleColumnLayoutSemanticHelper.getInstanceFor(oFCL, oSettings);
+      }
+    });
+  }
 );
